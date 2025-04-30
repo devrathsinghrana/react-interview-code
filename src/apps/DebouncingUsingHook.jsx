53194cccff10e9774355273ad5bc925af82706fb
devrathrana <devrath.rana@bold.com>
2025-05-01 00:20:51 +0530

    component updates

--- src/apps/DebouncingUsingHook.jsx
+++ src/apps/DebouncingUsingHook.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useDebounce from "../hooks/useDebounce";
 
 const DebouncingUsingHook = () => {
   return (
@@ -18,6 +19,7 @@ export default DebouncingUsingHook;
 
 function SearchValue() {
   const [query, setQuery] = useState("");
+  //   custom hooks themselves don’t have a lifecycle — but the hooks they call inside them do, and their cleanup will happen before the component is unmounted, just like with regular hooks.
   const debouncedQuery = useDebounce(query, 500); // For input state
 
   useEffect(() => {
@@ -30,13 +32,9 @@ function SearchValue() {
 }
 
 function SearchFunction() {
-  const debouncedFn = useDebounce(
-    (value) => {
-      console.log("Debounced API call with:", value);
-    },
-    500,
-    true
-  ); // Third argument `true` means "debounce as function"
+  const debouncedFn = useDebounce((value) => {
+    console.log("Debounced API call with:", value);
+  }, 500);
 
   return <input onChange={(e) => debouncedFn(e.target.value)} />;
 }
--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -4,7 +4,8 @@ export default function useDebounce(input, delay) {
   const [debouncedValue, setDebouncedValue] = useState(input);
   const timeoutRef = useRef(null);
 
-  const cancel = () => {
+  const cancel = (val) => {
+    console.log("val", val);
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
   };
 
@@ -21,13 +22,29 @@ export default function useDebounce(input, delay) {
 
   useEffect(() => {
     if (typeof input !== "function") {
-      cancel();
+      // It calls cancel("hey") again to be extra safe (clearing any old timeout before setting a new one).
+      cancel("hey"); //calling it here will not give issue as it will clear if any previous timeouts were attached to it
+
       timeoutRef.current = setTimeout(() => {
         setDebouncedValue(input);
       }, delay);
+      //   cancel("hey");//calling it here will give issue as user may never get debounce value returned to it
     }
 
-    return cancel;
+    // <-- this cleanup will be called BEFORE the next effect runs hence it is called before cancel("hey")
+    /**
+     * 
+     * When a component mounts, Strict Mode will:
+
+        Mount the component.
+
+        Immediately unmount it.
+
+        Mount it again (for real).
+
+        thus we are seeing cancel output in page hard refresh but it is actually executing due to component unmount or dependency changes not on first mount.
+     */
+    return cancel; //this will ensure cancel runs only after state is changed due to update in query by user before timer is up and removes previous timer and on component unmount. it will not run before state update to give setTimeout time to attach to the onChange event else if it is called immediately it will not return debounce value
   }, [input, delay]);
 
   return typeof input === "function" ? debouncedFn : debouncedValue;
