5a3d82fe55fb90a3ac2b903662996fcaf4e42726
BugBuster <devrath.rana@bold.com>
2025-05-14 23:49:56 +0530

    make-cart-page

--- src/apps/CartPage.jsx
+++ src/apps/CartPage.jsx
@@ -1,67 +1,120 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const CartPage = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const getProducts = async () => {
-    const res = await fetch("https://fakestoreapi.com/products");
-    const productList = await res.json();
-    const cartList = await Promise.all(
-      productList.map(async (item) => {
-        const res = await fetch("https://fakestoreapi.com/products/" + item.id);
-        const productDetails = await res.json();
-        return {
-          ...productDetails,
-          qty: 0,
-        };
-      })
-    );
-    setCartItems(cartList);
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const handleAddItem = (itemId) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === itemId ? { ...item, qty: item.qty + 1 } : item
-      )
-    );
-  };
-
-  const handleRemoveItem = (itemId) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === itemId && item.qty > 0
-          ? { ...item, qty: item.qty - 1 }
-          : item
-      )
-    );
-  };
-
-  const Card = ({ product }) => {
-    return (
-      <div>
-        <p>
-          <img height={20} width={20} src={product.image} alt="product-item" />
-        </p>
-        <p>{product.title}</p>
-        <p>{product.price}</p>
-        <p>
-          <button onClick={() => handleRemoveItem(product.id)}>-</button>
-          {product.qty}
-          <button onClick={() => handleAddItem(product.id)}>+</button>
-        </p>
-      </div>
-    );
-  };
-
+const Card = (props) => {
   return (
     <div>
-      {cartItems.map((cartItem) => (
-        <Card key={cartItem.id} product={cartItem} />
-      ))}
+      <p>{props.title}</p>
+      <img src={props.image} height={100} width={100} />
+      <p>
+        <button onClick={props.removeItem}>-</button>
+        {props.itemCount}
+        <button onClick={props.addItem}>+</button>
+      </p>
+    </div>
+  );
+};
+
+const reducerFn = (state, action) => {
+  switch (action.type) {
+    case "GET_CART_PRODUCTS":
+      return { ...state, cart: action.payload };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: (() =>
+          state.cart.map((item) => {
+            return {
+              ...item,
+              ...(item.id === action.payload && {
+                itemCount: item.itemCount + 1,
+              }),
+            };
+          }))(),
+      };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: (() =>
+          state.cart.map((item) => {
+            return {
+              ...item,
+              ...(item.id === action.payload &&
+                item.itemCount > 0 && {
+                  itemCount: item.itemCount - 1,
+                }),
+            };
+          }))(),
+      };
+    case "LOADING_STATE":
+      return { ...state, isLoading: action.payload };
+    default:
+      return state;
+  }
+};
+const CartPage = () => {
+  const [cart, setCart] = React.useReducer(reducerFn, {
+    cart: [],
+    isLoading: false,
+  });
+  function logErrors(...args) {
+    console.log(`${this.er} ${[...args]}`);
+  }
+  const getAllProducts = async () => {
+    try {
+      // throw new Error("API FAILED!")
+      setCart({ type: "LOADING_STATE", payload: true });
+      const res = await fetch("https://fakestoreapi.com/products");
+      const allProducts = await res.json();
+      const cartProducts = await Promise.all(
+        allProducts.map(async (product) => {
+          const res = await fetch(
+            `https://fakestoreapi.com/products/${product.id}`
+          );
+          const productDetails = await res.json();
+          return { ...productDetails, itemCount: 0 };
+        })
+      );
+      setCart({ type: "GET_CART_PRODUCTS", payload: cartProducts });
+      setCart({ type: "LOADING_STATE", payload: false });
+    } catch (er) {
+      setCart({ type: "LOADING_STATE", payload: false });
+      setCart({ type: "GET_CART_PRODUCTS", payload: [] });
+      logErrors.apply({ er }, ["error looged", "cartPage", "empty the cart"]);
+    }
+  };
+
+  React.useEffect(() => {
+    getAllProducts();
+    return () => setCart([]);
+  }, []);
+
+  function handleAddToCart() {
+    setCart({
+      type: "ADD_TO_CART",
+      payload: this.id,
+    });
+  }
+  function removeFromCart() {
+    setCart({
+      type: "REMOVE_FROM_CART",
+      payload: this.id,
+    });
+  }
+
+  if (cart.isLoading) return <p>Loading...</p>;
+  return (
+    <div>
+      <ul>
+        {cart.cart.length > 0 &&
+          cart.cart.map((cartItem) => (
+            <Card
+              key={cartItem.id}
+              {...cartItem}
+              removeItem={() => removeFromCart.call(cartItem)}
+              addItem={() => handleAddToCart.call(cartItem)}
+            />
+          ))}
+      </ul>
     </div>
   );
 };
